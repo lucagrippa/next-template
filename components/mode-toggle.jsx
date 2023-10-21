0daffd27db239a43e7e18798c5205a133ebc2557
lucagrippa <lucagrip@yahoo.com>
2023-10-20 21:55:36 -0400

    changed dark mode from dropdown menu to button

--- components/mode-toggle.jsx
+++ components/mode-toggle.jsx
@@ -1,42 +1,19 @@
 "use client"
-
 import * as React from "react"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { Sun, Moon } from 'lucide-react';
 
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="scale-100 dark:scale-0 text-amber-600" size={20} strokeWidth={1} />
-          <Moon className="absolute scale-0 dark:scale-100 text-yellow-200 " size={20} strokeWidth={1} />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={() => theme === 'light' ? setTheme("dark") : setTheme("light")}>
+      <Sun className="scale-100 dark:scale-0 text-amber-600" size={20} strokeWidth={1} />
+      <Moon className="absolute scale-0 dark:scale-100 text-yellow-200 " size={20} strokeWidth={1} />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
